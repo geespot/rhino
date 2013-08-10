f69e217d778c862a6ec9ef4872c19761d6fae66b
gxg8816 <gxg8816@gmail.com>
2013-08-10 23:47:38 +0800

    新范达城页面设计
    move jpg into sub folder
    added html pages

---
+++ docs/UI Mockup/新范达城页面框架/html/js/jquery-tab.js
@@ -0,0 +1,6 @@
+﻿$(function () {
+	$('.tabTitle li').click(function () {
+		$(this).addClass("tabin").siblings().removeClass();
+		$(".tabContent > ul").eq($(".tabTitle li").index(this)).show().siblings().hide();
+	});
+});
